56c7f07e4980e5d5007e2e7014d3bae0ea8c32c3
Jason Thorne <jrthorne@randalds.com.au>
2014-10-18 15:49:16 +1100

    Quite a few changes. Most notably the servers have been added. also made a new model for logs.

--- serverstatus/admin.py
+++ serverstatus/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import *
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status']
+# end ServerAdmin
 
 # Register your models here.
+admin.site.register(Server, ServerAdmin)
+admin.site.register(Check)
--- serverstatus/models.py
+++ serverstatus/models.py
@@ -2,4 +2,31 @@ from django.db import models
 
 # Create your models here.
 class Server(models.Model):
+    name            = models.CharField(max_length=255)
+    domain          = models.CharField(max_length=255, blank=True)
+    ip              = models.GenericIPAddressField(null=True, blank=True)
+    description     = models.TextField(blank=True)
+    
+    def __unicode__(self):
+        return self.name
+    # end unicode
+    
+    def status(self):
+        return True
+    # end status
+    status.boolean = True
+# end Server
+
+class Check(models.Model):
+    server          = models.ForeignKey('Server')
+    name            = models.CharField(max_length=255, blank=True)
+    command         = models.CharField(max_length=255, blank=True)
+    performed       = models.DateTimeField(auto_now=True)
+    returned_value  = models.TextField()
+    
+    def __unicode__(self):
+        return self.server.name + "-" + self.performed.strftime("%d %h %Y")
+    # end unicode
+# end check
+    
     
